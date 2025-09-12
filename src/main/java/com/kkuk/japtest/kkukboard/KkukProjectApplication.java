e7853dc4b5a48ee17740fd85d47bd0feba032f43
Kukyeon <rnrdus2000@naver.com>
2025-09-12 14:09:34 +0900

    oracle 클라우드 파일 수정

--- src/main/java/com/kkuk/japtest/kkukboard/KkukProjectApplication.java
+++ src/main/java/com/kkuk/japtest/kkukboard/KkukProjectApplication.java
@@ -2,9 +2,17 @@ package com.kkuk.japtest.kkukboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KkukProjectApplication {
+public class KkukProjectApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(KkukProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KkukProjectApplication.class, args);
--- src/main/java/com/kkuk/japtest/kkukboard/MainController.java
+++ src/main/java/com/kkuk/japtest/kkukboard/MainController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@GetMapping(value = "/") //root 요청 처리
+	@GetMapping(value = "/SoVeryHungry") //클라우드용 요청처리
+	//@GetMapping(value = "/")//root 요청 처리 (로컬용)
 	public String root() {
 		return "redirect:/question/list";
 	}
