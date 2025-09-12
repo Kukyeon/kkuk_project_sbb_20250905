47d9a4b86294f1f302b7196ec4fcab4383b4b9a6
Kukyeon <rnrdus2000@naver.com>
2025-09-12 16:09:33 +0900

    게시글 min 수정

--- src/main/java/com/kkuk/japtest/kkukboard/question/QuestionForm.java
+++ src/main/java/com/kkuk/japtest/kkukboard/question/QuestionForm.java
@@ -11,12 +11,12 @@ public class QuestionForm { // 질문 글의 제목과 내용의 유효성 체�
 	
 	@NotEmpty(message = "제목은 필수 항목입니다.")//제목이 공란으로 들어오면 작동
 	@Size(max = 200, message = "제목은 최대 200글자 까지 허용됩니다.") // 제목이 최대 200글자 까지 허용
-	@Size(min = 5, message = "제목은 최소 5글자 이상입니다.") // 제목이 최소 5글자 이상
+	@Size(min = 1) // 제목이 최소 5글자 이상
 	private String subject;
 	
 	@NotEmpty(message = "내용은 필수 항목입니다.")//제목이 공란으로 들어오면 작동
 	@Size(max = 500, message = "내용은 최대 500글자 까지 허용됩니다.") // 제목이 최대 500글자 까지 허용
-	@Size(min = 5, message = "내용은 최소 5글자 이상입니다.") // 내용이 최소 5글자 이상
+	@Size(min = 1) // 내용이 최소 5글자 이상
 	private String content;
 	
 }
